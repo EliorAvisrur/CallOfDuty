5b2fb7d81048ffc5c908370448419580804c5593
EliorAvisrur <elior261200@gmail.com>
2025-05-04 09:45:00 +0300

    justice-board routes and tests

--- test/health.test.js
+++ test/health.test.js
@@ -1,16 +1,9 @@
-import {
-  afterAll,
-  beforeAll,
-  expect,
-  test,
-  jest,
-  beforeEach,
-} from "@jest/globals";
-import { MongoClient } from "mongodb";
-import { createFastifyApp } from "../src/server/app";
-import { MongoMemoryServer } from "mongodb-memory-server";
+import { afterAll, beforeAll, expect, test, jest } from '@jest/globals';
+import { MongoClient } from 'mongodb';
+import { createFastifyApp } from '../src/server/app';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
-describe("Test Health endpoints", () => {
+describe('Test Health endpoints', () => {
   let mongod;
   let client;
   let db;
@@ -29,27 +22,27 @@ describe("Test Health endpoints", () => {
   });
   test("GET /health should return {'status': 'ok'}", async () => {
     const res = await fastify.inject({
-      method: "GET",
-      url: "/health",
+      method: 'GET',
+      url: '/health'
     });
     expect(res.statusCode).toBe(200);
-    expect(res.json()).toEqual({ status: "ok" });
+    expect(res.json()).toEqual({ status: 'ok' });
   });
   test("GET /health/db should return {'status': 'ok'}", async () => {
     const res = await fastify.inject({
-      method: "GET",
-      url: "/health/db",
+      method: 'GET',
+      url: '/health/db'
     });
     expect(res.statusCode).toBe(200);
-    expect(res.json()).toEqual({ status: "ok" });
+    expect(res.json()).toEqual({ status: 'ok' });
   });
-  test("GET /health/db should return status 500 when there is a database error", async () => {
-    jest.spyOn(fastify.mongo.db, "command").mockRejectedValue(() => {
-      throw new Error("MongoDB connection failed");
+  test('GET /health/db should return status 500 when there is a database error', async () => {
+    jest.spyOn(fastify.mongo.db, 'command').mockRejectedValue(() => {
+      throw new Error('MongoDB connection failed');
     });
     const response = await fastify.inject({
-      method: "GET",
-      url: "/health/db",
+      method: 'GET',
+      url: '/health/db'
     });
     expect(response.statusCode).toBe(500);
   });
